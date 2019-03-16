fa3395c3ddfb85d8ccfaf005e6b6693c8fd540ba
Yu Ma <jasonmy419@gmail.com>
2019-03-15 20:49:50 -0700

    final commit

---
+++ app/src/main/java/com/laioffer/tinnews/common/TinNoSwipeViewPager.java
@@ -0,0 +1,33 @@
+package com.laioffer.tinnews.common;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import java.util.jar.Attributes;
+
+public class TinNoSwipeViewPager extends ViewPager {
+
+    public TinNoSwipeViewPager(Context context) {
+        super(context);
+    }
+
+    public TinNoSwipeViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+
+
+}
---
+++ app/src/main/java/com/laioffer/tinnews/retrofit/response/BaseResponse.java
@@ -0,0 +1,9 @@
+package com.laioffer.tinnews.retrofit.response;
+
+import java.util.List;
+
+public class BaseResponse {
+    public String status;
+    public int totalResults;
+    public List<News> articles;
+}
